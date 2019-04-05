888c642b5a530a1d88573c2e7f156f2fc3b9d16f
AM_Myrick <amyrick.820@gmail.com>
2019-04-05 12:42:42 -0400

    deletes unneeded files and creates MessageList component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MessageList from "./components/MessageList";
 import './App.css';
 
+const DUMMY_DATA = [
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+]
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: DUMMY_DATA,
+    }
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Title />
+        <MessageList messages={this.state.messages} />
+        <SendMessageForm />
       </div>
     );
   }
